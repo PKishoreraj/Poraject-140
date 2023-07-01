b66b8c27df5650c5a03f81c9b24647ebdfeeccf0
PKishoreraj <113040359+PKishoreraj@users.noreply.github.com>
2023-07-01 19:28:45 +0530

    Add files via upload

--- g.js
+++ g.js
@@ -1,16 +1,35 @@
 song="";
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
 function preload(){
     song=loadSound("music.mp3");
 }
 function setup(){
     canvas=createCanvas(600,500);
-    canvas.center()
+    canvas.center();
     video=createCapture(VIDEO);
     video.hide;
+    poseNet=ml5.poseNet(video,moldel);
+    poseNet.on('pose',gotPoses);
 }
 function draw(){
     image(vedio,0,0,600,500);
 }
 function play(){
-    song.play()
+    song.play();
+    song.volume(1);
+    song.rate(1);
+}
+function gotPoses(results){
+    if(results.length>0){
+            console.log(results);
+            leftWristX=results[0].pose.leftWrist.x;
+            leftWristY=results[0].pose.leftWrist.y;
+            console.log("LeftWristX="+leftWristX+"leftWristY="+leftWristY);
+            rightWristX=results[0].pose.rightWrist.x;
+            rightWristY=results[0].pose.rightWrist.y;
+            console.log("RightWristX="+rightWristX+"rightWristY="+rightWristY);
+    }
 }
